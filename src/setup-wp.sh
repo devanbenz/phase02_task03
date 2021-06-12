c42e8587bba5fd319d85a9a81bea07cda2252d2a
devanbenz <devandbenz@gmail.com>
2021-06-11 22:26:33 -0500

    Spaces in env var where interfering with wp install

--- src/setup-wp.sh
+++ src/setup-wp.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 sleep 30
 wp core install --url=$URL --title=$WP_TITLE --admin_user=$WP_ADM --admin_password=$WP_PW --admin_email=$ADM_EMAIL --path=$WP_PATH --allow-root
-
 sleep 1
 wp plugin install $WP_PLUGIN --activate --allow-root --path=$WP_PATH
 sleep 1
